5ae2f9f8852eaa8588fadc5a4c2dec6dd88b379e
Fernando Herrera <fherrera0206@gmail.com>
2021-12-14 00:51:52 -0500

    Fix the navigation when click on navs

--- main.js
+++ main.js
@@ -3,20 +3,46 @@ const links = document.querySelector('.desktop-header');
 const hamb = document.querySelectorAll('.hamb');
 const nameH = document.querySelector('.name');
 const header = document.querySelector('.header');
+const hamb1 = document.querySelector('.link');
+const hamb2 = document.querySelector('.link-2');
+const hamb3 = document.querySelector('.link-3');
 
 
 mobileMenu.addEventListener('click', () => {  
   links.classList.toggle('activated');
   hamb.forEach(child => {
-      child.classList.toggle('activated')
-    })
-  
-    nameH.classList.toggle('activated')
-  
-    header.classList.toggle('activated')
+    child.classList.toggle('activated');
+  });  
+  nameH.classList.toggle('activated');  
+  header.classList.toggle('activated');
 });
   
+hamb1.addEventListener('click', () => {
+  links.classList.toggle('activated');
+  header.classList.toggle('activated');
+  nameH.classList.toggle('activated');  
+  hamb.forEach(child => {
+    child.classList.toggle('activated');
+  }); 
+})
 
+hamb2.addEventListener('click', () => {
+  links.classList.toggle('activated');
+  header.classList.toggle('activated');
+  nameH.classList.toggle('activated');  
+  hamb.forEach(child => {
+    child.classList.toggle('activated');
+  }); 
+})
+
+hamb3.addEventListener('click', () => {
+  links.classList.toggle('activated');
+  header.classList.toggle('activated');
+  nameH.classList.toggle('activated');  
+  hamb.forEach(child => {
+    child.classList.toggle('activated');
+  }); 
+})
 
 
 
