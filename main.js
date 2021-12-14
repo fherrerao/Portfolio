050a6586b005cade7659e07c23447d27dfe331b1
sergiogval <sergioval@gmail.com>
2021-12-13 21:11:01 -0600

    Fix js and css bugs

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const mobileMenu = document.querySelector('.mobile-header');
 const links = document.querySelector('.desktop-header');
+const hamb = document.querySelectorAll('.hamb');
+const nameH = document.querySelector('.name');
+const header = document.querySelector('.header');
 
 
 mobileMenu.addEventListener('click', () => {  
   links.classList.toggle('activated');
+  hamb.forEach(child => {
+      child.classList.toggle('activated')
+    })
   
+    nameH.classList.toggle('activated')
   
+    header.classList.toggle('activated')
 });
+  
+
+
+
+
+
+
+
+
