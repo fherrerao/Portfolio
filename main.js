6bcbf3f27600f27eaf9ca2c7cbe6db7c475680fb
sergiogval <sergioval@gmail.com>
2021-12-15 08:59:57 -0600

    Add modal

--- main.js
+++ main.js
@@ -125,3 +125,104 @@ liH3.appendChild(liTag3);
 
 const aH = document.querySelector('.projects-container');
 aH.appendChild(aTag);
+
+//Creating the modal window
+
+const modalContainer = document.createElement('DIV');
+modalContainer.classList.add('modal-container');
+
+const headerContainer = document.createElement('DIV');
+headerContainer.classList.add('header-container');
+
+const title = document.createElement('H2');
+title.classList.add('title-modal');
+title.textContent = project01.names;
+
+const closeButton = document.createElement('IMG');
+closeButton.classList.add('close-button');
+closeButton.src = 'img/close.svg';
+
+const techsContainer = document.createElement('UL');
+techsContainer.classList.add('techs-container');
+techsContainer.id = 'techs-container';
+
+const techs1 = document.createElement('LI');
+techs1.classList.add('techs');
+techs1.textContent = project01.technologies[0];
+
+const techs2 = document.createElement('LI');
+techs2.classList.add('techs');
+techs2.textContent = project01.technologies[1];
+
+const techs3 = document.createElement('LI');
+techs3.classList.add('techs');
+techs3.textContent = project01.technologies[2];
+
+const imageModal = document.createElement('IMG');
+imageModal.classList.add('img-modal');
+imageModal.src = project01.image;
+
+const paragraph = document.createElement('P');
+paragraph.classList.add('paragraph');
+paragraph.textContent = project01.description;
+
+const buttonContainer = document.createElement('DIV');
+buttonContainer.classList.add('button-container');
+
+const buttonLive = document.createElement('A');
+buttonLive.classList.add('button-project');
+buttonLive.textContent = 'See Live';
+buttonLive.href = project01.live;
+
+const buttonSource = document.createElement('A');
+buttonSource.classList.add('button-project');
+buttonSource.textContent = 'See Source';
+buttonSource.href = project01.source;
+
+const modalContainerH = document.querySelector('.projects');
+modalContainerH.appendChild(modalContainer);
+
+const headerContainerH = document.querySelector('.modal-container');
+headerContainerH.appendChild(headerContainer);
+
+const titleH = document.querySelector('.header-container');
+titleH.appendChild(title);
+
+const techsContainerH = document.querySelector('.modal-container');
+techsContainerH.appendChild(techsContainer);
+
+const techs1H = document.querySelector('#techs-container');
+techs1H.appendChild(techs1);
+
+const techs2H = document.querySelector('#techs-container');
+techs2H.appendChild(techs2);
+
+const techs3H = document.querySelector('#techs-container');
+techs3H.appendChild(techs3);
+
+const imageModalH = document.querySelector('.modal-container');
+imageModalH.appendChild(imageModal);
+
+const paragraphH = document.querySelector('.modal-container');
+paragraphH.appendChild(paragraph);
+
+const buttonContainerH = document.querySelector('.modal-container');
+buttonContainerH.appendChild(buttonContainer);
+
+const buttonLiveH = document.querySelector('.button-container');
+buttonLiveH.appendChild(buttonLive);
+
+const buttonSourceH = document.querySelector('.button-container');
+buttonSourceH.appendChild(buttonSource);
+
+const closeButtonH = document.querySelector('.header-container');
+closeButtonH.appendChild(closeButton);
+
+const clickButtonProject = document.querySelector('.button-project');
+clickButtonProject.addEventListener('click', () => {
+  modalContainer.classList.toggle('activated');  
+});
+
+closeButton.addEventListener('click', () => {
+  modalContainer.classList.toggle('activated');
+})
